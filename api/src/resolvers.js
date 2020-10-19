cccf8542c1ec5d498a42bcc571fcc96f03cdc5cc
akashgiricse <akashgiricse@gmail.com>
2020-10-19 16:32:32 +0530

    Add resolver for create and read results

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,12 +1,60 @@
 const Result = require("./models/Result");
 
-// const result = new Result({ name: "Zildjian" });
-// result.save().then(() => console.log("meow"));
-const { gql } = require("apollo-server");
-
 const resolvers = {
   Query: {
-    test: () => "Hi there",
+    scanResults: async (parent, args, context, info) => {
+      if (args.ids.length === 0) {
+        const results = await Result.find({});
+        return results;
+      } else {
+        console.log("bye");
+      }
+    },
+  },
+
+  Mutation: {
+    addScanResult: async (
+      parent,
+      { status, repositoryName, findings, finishedAt, queuedAt, scanningAt },
+      context,
+      info
+    ) => {
+      const result = new Result({
+        status,
+        repositoryName,
+        findings,
+        finishedAt,
+        queuedAt,
+        scanningAt,
+      });
+
+      await result.save();
+
+      if (result) {
+        obj = {
+          success: true,
+          message: "Successfuly saved scan result",
+          scanResult: [
+            {
+              id: result._id,
+              status: result.status,
+              repositoryName: result.repositoryName,
+              findings: result.findings,
+              queuedAt: result.queuedAt,
+              scanningAt: result.scanningAt,
+              finishedAt: result.finishedAt,
+            },
+          ],
+        };
+      } else {
+        obj = {
+          success: false,
+          message: "Could not save scan result",
+          scanResult: [null],
+        };
+      }
+      return obj;
+    },
   },
 };
 
